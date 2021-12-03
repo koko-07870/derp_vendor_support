4a63d6725a903fc23b9be6b733c509d458af0681
cjh1249131356 <cjh1249131356@gmail.com>
2021-12-03 15:46:42 +0800

    SwitchPreference: Respect haptic feedback switch
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- src/org/derpfest/support/preferences/SwitchPreference.java
+++ src/org/derpfest/support/preferences/SwitchPreference.java
@@ -19,18 +19,21 @@ package org.derpfest.support.preferences;
 import android.content.Context;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 import androidx.core.content.res.TypedArrayUtils;
 
 public class SwitchPreference extends androidx.preference.SwitchPreference {
 
+    private final Context mContext;
     private final Vibrator mVibrator;
 
     public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr,
             int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        mContext = context;
     }
 
     public SwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -50,6 +53,9 @@ public class SwitchPreference extends androidx.preference.SwitchPreference {
     @Override
     protected void performClick(View view) {
         super.performClick(view);
-        mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0) {
+            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
+        }
     }
 }
