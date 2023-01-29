crazy-stuffs/vendor_support

util: Import some utils from LineageOS
* FileUtils, ImageUtils and ScreenType

Change-Id: I689c22c822f6d89301e4f24924a0966d75b2f376
Signed-off-by: Mohammad Hasan Keramat J <ikeramat80@gmail.com>

src/com/libremobileos/support/util/FileUtils.java
@@ -1,0 +1,161 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2020 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.support.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class FileUtils {
+    private static final String TAG = "FileUtils";
+
+    private FileUtils() {
+        // This class is not supposed to be instantiated
+    }
+
+    /**
+     * Reads the first line of text from the given file.
+     * Reference {@link BufferedReader#readLine()} for clarification on what a line is
+     *
+     * @return the read line contents, or null on failure
+     */
+    public static String readOneLine(String fileName) {
+        String line = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName), 512);
+            line = reader.readLine();
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for reading", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not read from file " + fileName, e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return line;
+    }
+
+    /**
+     * Writes the given value into the given file
+     *
+     * @return true on success, false on failure
+     */
+    public static boolean writeLine(String fileName, String value) {
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(value);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for writing", e);
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Could not write to file " + fileName, e);
+            return false;
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks whether the given file exists
+     *
+     * @return true if exists, false if not
+     */
+    public static boolean fileExists(String fileName) {
+        final File file = new File(fileName);
+        return file.exists();
+    }
+
+    /**
+     * Checks whether the given file is readable
+     *
+     * @return true if readable, false if not
+     */
+    public static boolean isFileReadable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canRead();
+    }
+
+    /**
+     * Checks whether the given file is writable
+     *
+     * @return true if writable, false if not
+     */
+    public static boolean isFileWritable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canWrite();
+    }
+
+    /**
+     * Deletes an existing file
+     *
+     * @return true if the delete was successful, false if not
+     */
+    public static boolean delete(String fileName) {
+        final File file = new File(fileName);
+        boolean ok = false;
+        try {
+            ok = file.delete();
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to delete " + fileName, e);
+        }
+        return ok;
+    }
+
+    /**
+     * Renames an existing file
+     *
+     * @return true if the rename was successful, false if not
+     */
+    public static boolean rename(String srcPath, String dstPath) {
+        final File srcFile = new File(srcPath);
+        final File dstFile = new File(dstPath);
+        boolean ok = false;
+        try {
+            ok = srcFile.renameTo(dstFile);
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to rename " + srcPath + " to " + dstPath, e);
+        } catch (NullPointerException e) {
+            Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
+        }
+        return ok;
+    }
+}

src/com/libremobileos/support/util/ImageUtils.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2013-2014 The CyanogenMod Project
+* Copyright (C) 2020 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.support.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.util.Log;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+public class ImageUtils {
+    private static final String TAG = ImageUtils.class.getSimpleName();
+
+    private static final String ASSET_URI_PREFIX = "file:///android_asset/";
+    private static final int DEFAULT_IMG_QUALITY = 100;
+
+    /**
+     * Gets the Width and Height of the image
+     *
+     * @param inputStream The input stream of the image
+     *
+     * @return A point structure that holds the Width and Height (x and y)/*"
+     */
+    public static Point getImageDimension(InputStream inputStream) {
+        if (inputStream == null) {
+            throw new IllegalArgumentException("'inputStream' cannot be null!");
+        }
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(inputStream, null, options);
+        Point point = new Point(options.outWidth,options.outHeight);
+        return point;
+    }
+
+    /**
+     * Crops the input image and returns a new InputStream of the cropped area
+     *
+     * @param inputStream The input stream of the image
+     * @param imageWidth Width of the input image
+     * @param imageHeight Height of the input image
+     * @param inputStream Desired Width
+     * @param inputStream Desired Width
+     *
+     * @return a new InputStream of the cropped area/*"
+     */
+    public static InputStream cropImage(InputStream inputStream, int imageWidth, int imageHeight,
+            int outWidth, int outHeight) throws IllegalArgumentException {
+        if (inputStream == null){
+            throw new IllegalArgumentException("inputStream cannot be null");
+        }
+
+        if (imageWidth <= 0 || imageHeight <= 0) {
+            throw new IllegalArgumentException(
+                    String.format("imageWidth and imageHeight must be > 0: imageWidth=%d" +
+                            " imageHeight=%d", imageWidth, imageHeight));
+        }
+
+        if (outWidth <= 0 || outHeight <= 0) {
+            throw new IllegalArgumentException(
+                    String.format("outWidth and outHeight must be > 0: outWidth=%d" +
+                            " outHeight=%d", imageWidth, outHeight));
+        }
+
+        int scaleDownSampleSize = Math.min(imageWidth / outWidth, imageHeight / outHeight);
+        if (scaleDownSampleSize > 0) {
+            imageWidth /= scaleDownSampleSize;
+            imageHeight /= scaleDownSampleSize;
+        } else {
+            float ratio = (float) outWidth / outHeight;
+            if (imageWidth < imageHeight * ratio) {
+                outWidth = imageWidth;
+                outHeight = (int) (outWidth / ratio);
+            } else {
+                outHeight = imageHeight;
+                outWidth = (int) (outHeight * ratio);
+            }
+        }
+        int left = (imageWidth - outWidth) / 2;
+        int top = (imageHeight - outHeight) / 2;
+        InputStream compressed = null;
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            if (scaleDownSampleSize > 1) {
+                options.inSampleSize = scaleDownSampleSize;
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream, null, options);
+            if (bitmap == null) {
+                return null;
+            }
+            Bitmap cropped = Bitmap.createBitmap(bitmap, left, top, outWidth, outHeight);
+            ByteArrayOutputStream tmpOut = new ByteArrayOutputStream(2048);
+            if (cropped.compress(Bitmap.CompressFormat.PNG, DEFAULT_IMG_QUALITY, tmpOut)) {
+                byte[] outByteArray = tmpOut.toByteArray();
+                compressed = new ByteArrayInputStream(outByteArray);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Exception " + e);
+        }
+        return compressed;
+    }
+}

src/com/libremobileos/support/util/ScreenType.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (c) 2020 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.libremobileos.support.util;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.DisplayInfo;
+import android.view.WindowManager;
+
+public class ScreenType {
+    // Device type reference
+    private static int sDeviceType = -1;
+
+    // Device types
+    private static final int DEVICE_PHONE = 0;
+    private static final int DEVICE_HYBRID = 1;
+    private static final int DEVICE_TABLET = 2;
+
+    private static int getScreenType(Context context) {
+        if (sDeviceType == -1) {
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            DisplayInfo outDisplayInfo = new DisplayInfo();
+            wm.getDefaultDisplay().getDisplayInfo(outDisplayInfo);
+            int shortSize = Math.min(outDisplayInfo.logicalHeight, outDisplayInfo.logicalWidth);
+            int shortSizeDp = shortSize * DisplayMetrics.DENSITY_DEFAULT
+                    / outDisplayInfo.logicalDensityDpi;
+            if (shortSizeDp < 600) {
+                // 0-599dp: "phone" UI with a separate status & navigation bar
+                sDeviceType =  DEVICE_PHONE;
+            } else if (shortSizeDp < 720) {
+                // 600-719dp: "phone" UI with modifications for larger screens
+                sDeviceType = DEVICE_HYBRID;
+            } else {
+                // 720dp: "tablet" UI with a single combined status & navigation bar
+                sDeviceType = DEVICE_TABLET;
+            }
+        }
+        return sDeviceType;
+    }
+
+    public static boolean isPhone(Context context) {
+        return getScreenType(context) == DEVICE_PHONE;
+    }
+
+    public static boolean isHybrid(Context context) {
+        return getScreenType(context) == DEVICE_HYBRID;
+    }
+
+    public static boolean isTablet(Context context) {
+        return getScreenType(context) == DEVICE_TABLET;
+    }
+}

koko-07870/derp_vendor_support

src/org/derpfest/support/util/FileUtils.java
@@ -1,0 +1,161 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2020 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.support.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class FileUtils {
+    private static final String TAG = "FileUtils";
+
+    private FileUtils() {
+        // This class is not supposed to be instantiated
+    }
+
+    /**
+     * Reads the first line of text from the given file.
+     * Reference {@link BufferedReader#readLine()} for clarification on what a line is
+     *
+     * @return the read line contents, or null on failure
+     */
+    public static String readOneLine(String fileName) {
+        String line = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName), 512);
+            line = reader.readLine();
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for reading", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not read from file " + fileName, e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return line;
+    }
+
+    /**
+     * Writes the given value into the given file
+     *
+     * @return true on success, false on failure
+     */
+    public static boolean writeLine(String fileName, String value) {
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(value);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for writing", e);
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Could not write to file " + fileName, e);
+            return false;
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks whether the given file exists
+     *
+     * @return true if exists, false if not
+     */
+    public static boolean fileExists(String fileName) {
+        final File file = new File(fileName);
+        return file.exists();
+    }
+
+    /**
+     * Checks whether the given file is readable
+     *
+     * @return true if readable, false if not
+     */
+    public static boolean isFileReadable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canRead();
+    }
+
+    /**
+     * Checks whether the given file is writable
+     *
+     * @return true if writable, false if not
+     */
+    public static boolean isFileWritable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canWrite();
+    }
+
+    /**
+     * Deletes an existing file
+     *
+     * @return true if the delete was successful, false if not
+     */
+    public static boolean delete(String fileName) {
+        final File file = new File(fileName);
+        boolean ok = false;
+        try {
+            ok = file.delete();
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to delete " + fileName, e);
+        }
+        return ok;
+    }
+
+    /**
+     * Renames an existing file
+     *
+     * @return true if the rename was successful, false if not
+     */
+    public static boolean rename(String srcPath, String dstPath) {
+        final File srcFile = new File(srcPath);
+        final File dstFile = new File(dstPath);
+        boolean ok = false;
+        try {
+            ok = srcFile.renameTo(dstFile);
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to rename " + srcPath + " to " + dstPath, e);
+        } catch (NullPointerException e) {
+            Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
+        }
+        return ok;
+    }
+}

src/org/derpfest/support/util/ImageUtils.java
@@ -1,0 +1,118 @@
+/*
+ * Copyright (C) 2013-2014 The CyanogenMod Project
+* Copyright (C) 2020 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.support.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.util.Log;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+public class ImageUtils {
+    private static final String TAG = ImageUtils.class.getSimpleName();
+
+    private static final String ASSET_URI_PREFIX = "file:///android_asset/";
+    private static final int DEFAULT_IMG_QUALITY = 100;
+
+    /**
+     * Gets the Width and Height of the image
+     *
+     * @param inputStream The input stream of the image
+     *
+     * @return A point structure that holds the Width and Height (x and y)/*"
+     */
+    public static Point getImageDimension(InputStream inputStream) {
+        if (inputStream == null) {
+            throw new IllegalArgumentException("'inputStream' cannot be null!");
+        }
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(inputStream, null, options);
+        Point point = new Point(options.outWidth,options.outHeight);
+        return point;
+    }
+
+    /**
+     * Crops the input image and returns a new InputStream of the cropped area
+     *
+     * @param inputStream The input stream of the image
+     * @param imageWidth Width of the input image
+     * @param imageHeight Height of the input image
+     * @param inputStream Desired Width
+     * @param inputStream Desired Width
+     *
+     * @return a new InputStream of the cropped area/*"
+     */
+    public static InputStream cropImage(InputStream inputStream, int imageWidth, int imageHeight,
+            int outWidth, int outHeight) throws IllegalArgumentException {
+        if (inputStream == null){
+            throw new IllegalArgumentException("inputStream cannot be null");
+        }
+
+        if (imageWidth <= 0 || imageHeight <= 0) {
+            throw new IllegalArgumentException(
+                    String.format("imageWidth and imageHeight must be > 0: imageWidth=%d" +
+                            " imageHeight=%d", imageWidth, imageHeight));
+        }
+
+        if (outWidth <= 0 || outHeight <= 0) {
+            throw new IllegalArgumentException(
+                    String.format("outWidth and outHeight must be > 0: outWidth=%d" +
+                            " outHeight=%d", imageWidth, outHeight));
+        }
+
+        int scaleDownSampleSize = Math.min(imageWidth / outWidth, imageHeight / outHeight);
+        if (scaleDownSampleSize > 0) {
+            imageWidth /= scaleDownSampleSize;
+            imageHeight /= scaleDownSampleSize;
+        } else {
+            float ratio = (float) outWidth / outHeight;
+            if (imageWidth < imageHeight * ratio) {
+                outWidth = imageWidth;
+                outHeight = (int) (outWidth / ratio);
+            } else {
+                outHeight = imageHeight;
+                outWidth = (int) (outHeight * ratio);
+            }
+        }
+        int left = (imageWidth - outWidth) / 2;
+        int top = (imageHeight - outHeight) / 2;
+        InputStream compressed = null;
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            if (scaleDownSampleSize > 1) {
+                options.inSampleSize = scaleDownSampleSize;
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream, null, options);
+            if (bitmap == null) {
+                return null;
+            }
+            Bitmap cropped = Bitmap.createBitmap(bitmap, left, top, outWidth, outHeight);
+            ByteArrayOutputStream tmpOut = new ByteArrayOutputStream(2048);
+            if (cropped.compress(Bitmap.CompressFormat.PNG, DEFAULT_IMG_QUALITY, tmpOut)) {
+                byte[] outByteArray = tmpOut.toByteArray();
+                compressed = new ByteArrayInputStream(outByteArray);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Exception " + e);
+        }
+        return compressed;
+    }
+}

src/org/derpfest/support/util/ScreenType.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (c) 2020 The LibreMobileOS Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.support.util;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.DisplayInfo;
+import android.view.WindowManager;
+
+public class ScreenType {
+    // Device type reference
+    private static int sDeviceType = -1;
+
+    // Device types
+    private static final int DEVICE_PHONE = 0;
+    private static final int DEVICE_HYBRID = 1;
+    private static final int DEVICE_TABLET = 2;
+
+    private static int getScreenType(Context context) {
+        if (sDeviceType == -1) {
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            DisplayInfo outDisplayInfo = new DisplayInfo();
+            wm.getDefaultDisplay().getDisplayInfo(outDisplayInfo);
+            int shortSize = Math.min(outDisplayInfo.logicalHeight, outDisplayInfo.logicalWidth);
+            int shortSizeDp = shortSize * DisplayMetrics.DENSITY_DEFAULT
+                    / outDisplayInfo.logicalDensityDpi;
+            if (shortSizeDp < 600) {
+                // 0-599dp: "phone" UI with a separate status & navigation bar
+                sDeviceType =  DEVICE_PHONE;
+            } else if (shortSizeDp < 720) {
+                // 600-719dp: "phone" UI with modifications for larger screens
+                sDeviceType = DEVICE_HYBRID;
+            } else {
+                // 720dp: "tablet" UI with a single combined status & navigation bar
+                sDeviceType = DEVICE_TABLET;
+            }
+        }
+        return sDeviceType;
+    }
+
+    public static boolean isPhone(Context context) {
+        return getScreenType(context) == DEVICE_PHONE;
+    }
+
+    public static boolean isHybrid(Context context) {
+        return getScreenType(context) == DEVICE_HYBRID;
+    }
+
+    public static boolean isTablet(Context context) {
+        return getScreenType(context) == DEVICE_TABLET;
+    }
+}