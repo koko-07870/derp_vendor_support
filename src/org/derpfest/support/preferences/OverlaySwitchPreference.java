ad43de4a3bbd6de8d48d2ab6bc7d9074451a98d3
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-06 23:13:35 +0200

    OverlaySwitchPreference: SwitchPreference -> SwitchPreferenceCompat

--- src/org/derpfest/support/preferences/OverlaySwitchPreference.java
+++ src/org/derpfest/support/preferences/OverlaySwitchPreference.java
@@ -31,12 +31,12 @@ import android.os.UserHandle;
 import android.util.AttributeSet;
 import android.util.Log;
 
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import java.lang.SecurityException;
 import java.util.List;
 
-public class OverlaySwitchPreference extends SwitchPreference {
+public class OverlaySwitchPreference extends SwitchPreferenceCompat {
     private final static String TAG = "OverlaySwitchPreference";
     private final static String SETTINGSNS = "http://schemas.android.com/apk/res-auto";
     private static final String DKEY = "dkey";
